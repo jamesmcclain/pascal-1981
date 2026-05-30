456487d97354ebbf5fb30447a14b0583ceb62866
James W. McClain <jmcclain@daystrom-data-concepts.com>
2026-05-29 23:05:05 -0400

    Update test suite

---
+++ pascal_test_suite/should_fail/12_exponent_sign_no_digits.pas
@@ -0,0 +1,5 @@
+(* should_fail: exponent sign with no following digits. *)
+PROGRAM P;
+CONST C = 1.5E+;
+BEGIN
+END.
---
+++ pascal_test_suite/should_fail/13_bare_dollar.pas
@@ -0,0 +1,5 @@
+(* should_fail: '$' with no hex digits. *)
+PROGRAM P;
+CONST C = $;
+BEGIN
+END.
